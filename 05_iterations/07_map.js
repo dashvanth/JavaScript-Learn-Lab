39fff351e94b6c4caf1d0fc1ca0efc12706d4cc0
Dashvanth <dashvanthrajhc@gmail.com>
2023-08-11 15:21:41 +0000

    methods

---
+++ 05_iterations/07_map.js
@@ -0,0 +1,14 @@
+const myNumbers = [1,2,3,4,5,6,7,8,9,10];
+
+// const newNumbers = myNumbers.map( (num) =>{
+//     return num + 10;
+// })
+// console.log(newNumbers);
+
+// method chaining 
+// const newNumbers = 
+// const newNumbers = [];
+// myNumbers.forEach( (num) => {
+//     newNumbers.push( num +20)
+// })
+// console.log(newNumbers);
